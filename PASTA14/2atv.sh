51a7767014830b288e44a5e4e6e3b911c9e51846
Valeria Ramalho <valeria.ramalho2021@gmail.com>
2021-06-01 11:01:06 -0300

    PASTA13 - 2; Atividade, Array associativos

---
+++ PASTA14/2atv.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+declare -A chaves
+NEXT_IDX=1
+while read line; do
+    if [ "x${chaves[$line]}" = "x"]; then
+        chaves[$line]=$NEXT_IDX
+        NEXT_IDX=$(( NEXT_IDX + 1 ))
+        echo "$line:${chaves[$line]}"
+    fi
+done
+
+
+# executando ./e.sh << EOL
+#>bash
+#>is
+#>tought
+#>as
+#>bash
+#>is
+#>pretty
+#EOL
+#Saida do comando
+#bash:1
+#is:2
+#tought:3
+#as:4
+#pretty:5
+
