1da0c0c9f9f8f1c06d13900f216eda2328ef4289
Valeria Ramalho <valeria.ramalho2021@gmail.com>
2021-06-01 20:02:18 -0300

    PASTA14 - 1-;Atividade, Array e string.

--- PASTA14/1_maior_ do _array.sh
+++ PASTA14/1_maior_ do _array.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-# 1- Atividade
-function maior () {
-   for a in ${a[*]}; do
-       echo $a
-done
+# 1- Atividade, maior_array 
+function maior_array () {
+   total=${#num[@]}
+   
+   for (( i=0; i<"$total"; i++ )); do
+     if [[ "${num[i]}" -ge "$maior" ]]; then
+       maior=${num[i]}
+     fi
+   done
+   
+   echo "$maior"
 }
-read -p "Digite os numeros; " a
-echo "elementos " $a
+
+echo -e "\nDigite os números , dando espaços; "
+read -ra num 
+nmaior=$( maior_array ${num[@]} )
+echo -e "\n O maior é; $nmaior\n"
